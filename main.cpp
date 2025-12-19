ba4b158946c76990a64d647d16884dba7061053b
Legitmod17 <neshkin2018@yandex.ru>
2025-12-19 10:37:58 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+setlocale(0, "ru");
+srand(time(NULL));
+int menu;
+int start = 1;
+int max = 10;
+int chance = 5;
+int guess;
+int wins = 0;
+}
