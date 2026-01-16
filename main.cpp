e6317c299a24efbe3d7f9a44f1d5587cb6c6919c
Legitmod17 <neshkin2018@yandex.ru>
2026-01-16 14:55:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,29 +22,50 @@ int main() {
 	}
 
 	if (menu == 1) {
-	int num1, num2, num3;
-	num1 = rand() % max + start;
-
-	
-
-	do {
-		num2 = rand() % max + start;
-	} while (num3 == num1 || num3 = num3);
-
-	bool guessed1 = false;
-	bool guessed2 = false;
-	bool guesses2 = false;
-
-	while (chance > 0 && wins < 3) {
-
-	}
+		int num1, num2, num3;
+		num1 = rand() % max + start;
 
 
 
+		do {
+			num2 = rand() % max + start;
+		} while (num3 == num1 || num3 = num3);
+
+		bool guessed1 = false;
+		bool guessed2 = false;
+		bool guesses2 = false;
+
+		while (chance > 0 && wins < 3) {
+			cout << "[ + ] Угаданных чисел: [ " << wins << " / 3 ]" << endl;
+			cout << "[ + ] Попыток: [" << chance << ']' << endl;
+			cout << endl;
+			cout << "[>] Угадать число : ";
+			cin >> guess;
+
+
+			if (guess < 1 || guess > 10) {
+				cout << "\x1b[33m[!] Ошибка! Введите число от 1 до 10\x1b[0m" << endl;
+				cout << endl;
 
 
 
+				cin.clear();
+			    cin.ignore(10000, '\n');
+				continue;
+			}
+
+			if (!guessed1 && guess == num1) {
+				cout << "\x1bх[92m[ + ] Вы  угадали число!x1b[0m" << endl;
+				wins++;
+				guessed1 = true;
+			}
+
+			if (!guessed1 && guess == num2) {
+				cout << "\92m[ + ] Вы угадали число!x1b[0m" << endl;
+				wins++;
+				guessed1 = true;
+			}
 
 
 
-	}
+		}
