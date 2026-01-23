f753b3c4d6e52ad6348d023d71779a8966450c34
Legitmod17 <neshkin2018@yandex.ru>
2026-01-23 13:55:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -50,22 +50,43 @@ int main() {
 
 
 				cin.clear();
-			    cin.ignore(10000, '\n');
+				cin.ignore(10000, '\n');
 				continue;
 			}
 
-			if (!guessed1 && guess == num1) {
-				cout << "\x1bх[92m[ + ] Вы  угадали число!x1b[0m" << endl;
+			else if (!guessed1 && guess == num1) {
+				cout << "\x1bх[92m[ + ] Вы  угадали число!\x1b[0m" << endl;
 				wins++;
 				guessed1 = true;
 			}
 
-			if (!guessed1 && guess == num2) {
-				cout << "\92m[ + ] Вы угадали число!x1b[0m" << endl;
+			else if (!guessed2 && guess == num2) {
+				cout << "\92m[ + ] Вы угадали число!\x1b[0m" << endl;
 				wins++;
 				guessed1 = true;
 			}
 
+			else if (!guessed3 && guess == num3) {
+				cout << "\92m[ + ] Вы угадали число!"\x1b[0m" << endl;
+					wins++;
+				guessed3 = true;
+			}
 
+			else {
+				cout << "\x1b[31m[ - ] Вы не угадали число!\x1b[0m" << endl;
+				chance--;
+				cout << endl;
+			}
 
 		}
+
+		if (wins == 3) {
+			cout << "\x1b[92m[ ПОБЕДА! ] Вы угадали все 3 числа!x1b[0m" << endl;
+		}
+
+		else {
+			cout << "\x1b[31m[ Проигрыш ] У вас закончились попытки!\x1b[0m" << endl;
+		}
+	}
+	return 0;
+	}
